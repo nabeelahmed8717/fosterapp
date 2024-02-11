f5d0ade333bc699b5b72c6e2760af6789e3a548f
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2024-02-11 20:41:25 +0500

    adding project files

--- src/components/NotistackProvider.tsx
+++ src/components/NotistackProvider.tsx
@@ -1,15 +1,10 @@
-import { ReactNode, useRef } from "react";
-import { SnackbarProvider } from "notistack";
+
 // @mui
 import { alpha, useTheme } from "@mui/material/styles";
 import { Box, GlobalStyles, IconButton } from "@mui/material";
 
 // @mui icons
-import CloseIcon from "@mui/icons-material/Close";
-import ErrorIcon from "@mui/icons-material/Error";
-import ReportProblemIcon from "@mui/icons-material/ReportProblem";
-import CheckCircleIcon from "@mui/icons-material/CheckCircle";
-import InfoIcon from "@mui/icons-material/Info";
+
 // ----------------------------------------------------------------------
 
 function SnackbarStyles() {
@@ -54,29 +49,29 @@ function SnackbarStyles() {
 
 // ----------------------------------------------------------------------
 
-export default function NotistackProvider({
-  children,
-}: {
-  children: ReactNode;
-}) {
-  const notistackRef: any = useRef(null);
-  const theme = useTheme();
-  const iconColor = theme.palette.grey["100"];
+// export default function NotistackProvider({
+//   children,
+// }: {
+//   children: ReactNode;
+// }) {
+//   const notistackRef: any = useRef(null);
+//   const theme = useTheme();
+//   const iconColor = theme.palette.grey["100"];
 
-  const onClose = (key: string) => () => {
-    notistackRef.current.closeSnackbar(key);
-  };
+//   const onClose = (key: string) => () => {
+//     notistackRef.current.closeSnackbar(key);
+//   };
 
-  return (
-    <>
-      <SnackbarStyles />
+//   return (
+//     <>
+//       <SnackbarStyles />
 
-      <SnackbarProvider>
-        {children}
-      </SnackbarProvider>
-    </>
-  );
-}
+//       <SnackbarProvider>
+//         {children}
+//       </SnackbarProvider>
+//     </>
+//   );
+// }
 
 // ----------------------------------------------------------------------
 
